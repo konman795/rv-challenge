1af5c35df8386f4554e92f66db7f3ddc2225fefa
Kon-Kon Chao <kchao@vinformatix.com>
2018-10-05 10:25:50 -0500

    conditional logic for createElement function

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,34 @@
 const dealers = dealersData.dealers;
 let dealerFrag = document.createDocumentFragment();
 
-// for (let dealer of dealers) {
-//     let dealerSection = createElement('section', 'dealer', '');
-//     let dealerName = createElement('div', 'dealer__name', '');
-//     let dealerText = createElement('span', 'dealer__text', dealer.data.name);
+for (let dealer of dealers) {
+    let dealerSection = createElement('section', 'dealer', '');
+    let dealerName = createElement('div', 'dealer__name', '');
+    let dealerText = createElement('span', 'dealer__text', dealer.data.name);
 
-//     let seperatorGrey = createElement('div', 'seperator-grey', '');
+    let seperatorGrey = createElement('div', 'seperator-grey', '');
 
-//     dealerName.appendChild(dealerText);
+    dealerName.appendChild(dealerText);
 
-//     dealerSection.appendChild(dealerName);
-//     dealerSection.appendChild(seperatorGrey);
+    dealerSection.appendChild(dealerName);
+    dealerSection.appendChild(seperatorGrey);
     
-//     dealerFrag.appendChild(dealerSection);
-// }
-// document.querySelector('body').appendChild(dealerFrag);
+    dealerFrag.appendChild(dealerSection);
+}
+document.querySelector('body').appendChild(dealerFrag);
 
 function createElement(tagName, className, text) {
     let el = document.createElement(tagName);
-    el.classList.add(className);
-    el.innerText = text;
+    if (className.length > 0) {
+        el.classList.add(className);
+    }
+    if (text.length > 0) {
+        el.innerText = text;
+    }
     return el;
 }
+
+window.addEventListener('load', () => {
+    console.log('loaded all assets');
+    document.querySelector('body').style.animation = 'fadeIn 0.8s 1';
+});
