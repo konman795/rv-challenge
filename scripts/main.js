43db50ef4a4c5ba329a3992b9c728ed83fbf746d
Kon-Kon <konman795@gmail.com>
2018-10-09 21:42:34 -0500

    form validation complete, filtering complete

--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,38 @@
 const dealers = dealersData.dealers;
-let filteredDealers = dealersData.dealers;
-showResults(dealers);
 
-let filterOptions = [];
-const filterCheckboxes = document.querySelectorAll('.filters__options .custom-checkbox__input');
-for (let i = 0; i < filterCheckboxes.length; i++) {
-    filterCheckboxes[i].addEventListener('click', function() {
-        if (this.checked) {
-            filterOptions.push(this.value);
-        }
-        else {
-            for (let j = 0; j < filterOptions.length; j++) {
-                if (filterOptions[j] === this.value) {
-                    filterOptions.splice(j, 1);
-                }
+document.addEventListener('DOMContentLoaded', function() {
+    showResults(dealers);
+});
+
+(function initiateFilterOptions() {
+    let filteredDealers = dealersData.dealers;
+    let filterOptions = [];
+    const filterCheckboxes = document.querySelectorAll('.filters__options .custom-checkbox__input');
+    for (let i = 0; i < filterCheckboxes.length; i++) {
+        filterCheckboxes[i].addEventListener('click', function() {
+            if (this.checked) {
+                filterOptions.push(this.value);
             }
-        }
-        filteredDealers = dealers.filter(function(cert) {
-            // return filterOptions.indexOf(cert.data.certifications) >= 0;
+            else {
+                filterOptions.splice(filterOptions.indexOf(this.value), 1);
+            }
+            filteredDealers = dealers.filter(function(dealer) {
+                let dealerCerts = dealer.data.certifications;
+                return filterOptions.every(function(val) {
+                    return dealerCerts.indexOf(val) >= 0;
+                });
+            });
+            showResults(filteredDealers);
         });
-        showResults(dealers);
-    });
-}
+    }
+})()
 
 function showResults(dealers) {
     let dealerFrag = document.createDocumentFragment();
     const dealersContainer = document.querySelector('#dealers-container');
+    const dealerCount = document.querySelector('.filters__dealer-count');
+
+    dealerCount.innerText = dealers.length;
     
     for (let i = 0; i < dealers.length; i++) {
         let dealerSection = createElement('section', ['dealer'], '');
@@ -112,49 +119,50 @@ function showResults(dealers) {
 
     dealersContainer.innerHTML = "";
     dealersContainer.appendChild(dealerFrag);
-}
+    initiateModals();
 
-function createElement(tagName, classNamesArray, text) {
-    let el = document.createElement(tagName);
-    if (classNamesArray.length > 0) {
-        for (let i = 0; i < classNamesArray.length; i++) {
-            el.classList.add(classNamesArray[i]);
+    function createElement(tagName, classNamesArray, text) {
+        let el = document.createElement(tagName);
+        if (classNamesArray.length > 0) {
+            for (let i = 0; i < classNamesArray.length; i++) {
+                el.classList.add(classNamesArray[i]);
+            }
         }
-    }
-    if (text.length > 0) {
-        el.innerText = text;
-    }
-    return el;
-}
-
-function createImgElement(classNamesArray, imgPath) {
-    let el = document.createElement('img');
-    if (classNamesArray.length > 0) {
-        for (let i = 0; i < classNamesArray.length; i++) {
-            el.classList.add(classNamesArray[i]);
+        if (text.length > 0) {
+            el.innerText = text;
         }
+        return el;
     }
-    if (imgPath.length > 0) {
-        el.src = imgPath;
-    }
-    return el;
-}
-
-function createBtnElement(classNamesArray, dataSetValue) {
-    let el = document.createElement('button');
-    if (classNamesArray.length > 0) {
-        for (let i = 0; i < classNamesArray.length; i++) {
-            el.classList.add(classNamesArray[i]);
+    
+    function createImgElement(classNamesArray, imgPath) {
+        let el = document.createElement('img');
+        if (classNamesArray.length > 0) {
+            for (let i = 0; i < classNamesArray.length; i++) {
+                el.classList.add(classNamesArray[i]);
+            }
         }
+        if (imgPath.length > 0) {
+            el.src = imgPath;
+        }
+        return el;
     }
-    if (dataSetValue) {
-        el.dataset.companyId = dataSetValue;
+    
+    function createBtnElement(classNamesArray, dataSetValue) {
+        let el = document.createElement('button');
+        if (classNamesArray.length > 0) {
+            for (let i = 0; i < classNamesArray.length; i++) {
+                el.classList.add(classNamesArray[i]);
+            }
+        }
+        if (dataSetValue) {
+            el.dataset.companyId = dataSetValue;
+        }
+        el.type = 'button';
+        return el;
     }
-    el.type = 'button';
-    return el;
 }
 
-(function initiateModals() {
+function initiateModals() {
     const modal = document.querySelector('.modal');
     const closeModal = document.querySelector('.modal__close-btn');
     const dealerEmailBtns = document.querySelectorAll('.dealer__email-btn');
@@ -170,7 +178,7 @@ function createBtnElement(classNamesArray, dataSetValue) {
         modal.classList.remove('show');
         clearForm();
     });
-})()
+}
 
 function showModal(eventTarget, companyId) {
     const modal = document.querySelector('.modal');
@@ -189,17 +197,66 @@ function showModal(eventTarget, companyId) {
     }
 
     contactFormEmailRecipient.value = dealerObj.email;
+    formValidation();
     modal.classList.add('show');
 }
 
 function clearForm() {
     const contactForm = document.querySelector('#pool-pro-contact-form');
+    const formSubmitBtn = document.querySelector('.modal__form-submit-btn');
+    const formGroups = document.querySelectorAll('.form-group');
+    const formFields = document.querySelectorAll('.form-field');
     const contactFormEmailRecipient =  document.querySelector('input[name="emailRecipient"]');
 
+    for (let i = 0; i < formGroups.length; i++) {
+        formGroups[i].className = 'form-group';
+    }
+    for (let i = 0; i < formFields.length; i++) {
+        formFields[i].className = 'form-field';
+    }
     contactFormEmailRecipient.value = "";
+    formSubmitBtn.disabled = true;
     contactForm.reset();
 }
 
 function formValidation() {
-    
+    const fullNameField = document.querySelector('input[name="fullName"]');
+    const phoneNumberField = document.querySelector('input[name="phoneNumber"]');
+    const emailField = document.querySelector('input[name="email"]');
+
+    bindValidationEvents(fullNameField, RegExp("^[A-Za-z\\s]+$"));
+    bindValidationEvents(phoneNumberField, RegExp("^(\\+\\d{1,2}\\s)?\\(?\\d{3}\\)?[\\s.-]?\\d{3}[\\s.-]?\\d{4}$"));
+    bindValidationEvents(emailField, RegExp("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?"));
+
+    function bindValidationEvents(inputEl, regEx) {
+        inputEl.addEventListener('keypress', function() {
+            this.classList.add('touched');
+        });
+        inputEl.addEventListener('blur', function() {
+            if (this.classList.contains('touched')) {
+                if (!this.value || !regEx.test(this.value)) {
+                    this.parentElement.classList.add('invalid');
+                    this.parentElement.classList.remove('valid');
+                }
+                else  {
+                    this.parentElement.classList.remove('invalid');
+                    this.parentElement.classList.add('valid');
+                }
+                checkFormValid();
+            }
+        });
+    }
+
+    function checkFormValid() {
+        const formSubmitBtn = document.querySelector('.modal__form-submit-btn');
+        if ([fullNameField, phoneNumberField, emailField].every(
+            function(field) {
+                return field.parentElement.classList.contains('valid');
+            })) {
+            formSubmitBtn.disabled = false;
+        }
+        else {
+            formSubmitBtn.disabled = true;
+        }
+    };
 }
