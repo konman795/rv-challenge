cfbe15c506a4bcc9a66f239d4d1ec651bc79da60
Kon-Kon <konman795@gmail.com>
2018-10-13 15:09:35 -0500

    clean up

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-let gulp = require('gulp');
-let useref = require('gulp-userref');
-
-gulp.task('useref', function(){
-    return gulp.src('app/*.html')
-      .pipe(useref())
-      .pipe(gulp.dest('dist'))
-  });
